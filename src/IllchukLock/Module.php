9dcae0b11db8795dead48b31933bfa059347fbce
Derek Illchuk <dillchuk@gmail.com>
2017-08-27 13:41:42 -0500

    Fixing up config.

--- src/IllchukLock/Module.php
+++ src/IllchukLock/Module.php
@@ -7,7 +7,7 @@ use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 class Module implements AutoloaderProviderInterface {
 
     public function getConfig() {
-        return include __DIR__ . '/../config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     public function getAutoloaderConfig() {
